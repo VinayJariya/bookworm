e45887d07456f0ef063b1e8a56be2d493647c86a
Vinay Jariya <vinayjariya@gmail.com>
2017-07-13 18:25:40 +0530

    Static Pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+	# Returns full title for Pages
+	def full_title(title = "")
+		@base_title = "BookWorm: A Portal for BookWorms"
+		title.empty? ? @base_title : "#{title} | #{@base_title}"
+	end
 end
